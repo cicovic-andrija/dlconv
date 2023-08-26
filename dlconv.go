ebf43e5e534d7d08e5b38c7a4973fece4383da44
cicovic-andrija <cicovic.andrija@gmail.com>
2023-08-26 21:39:01 +0200

    Update comment

--- dlconv.go
+++ dlconv.go
@@ -15,7 +15,7 @@ const (
 	TemplateMarkdownText = `---
 tags: [diving, my]
 title: {{ .Title }}
-comment: 'This document is auto-generated from the master dive log file using https://github.com/cicovic-andrija/dlconv.'
+comment: 'This document is auto-generated from the master dive log using https://github.com/cicovic-andrija/dlconv.'
 created: '2023-04-16T20:36:59.779Z'
 modified: '{{ .ModifiedTimeUTC }}'
 ---
